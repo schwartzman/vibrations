811de4e945964da77c2110f370c8c0154999fc57
Anthony Schwartzman <anthony@anthonyschwartzman.com>
2016-01-08 12:28:05 +0700

    Enable Analytics

--- index.php
+++ index.php
@@ -40,12 +40,12 @@
 	    <?php endif; ?>
         <script src="/static/main.js"></script>
         <script>
-            // (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-            // function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-            // e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-            // e.src='//www.google-analytics.com/analytics.js';
-            // r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-            // ga('create','<?=ID_GA?>','auto');ga('send','pageview');
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','<?=ID_GA?>','auto');ga('send','pageview');
         </script>
     </body>
 </html>
