66195246eab6008ab6328728ef472621e9994033
Anthony Schwartzman <anthony@anthonyschwartzman.com>
2018-06-26 19:03:14 +0700

    Set up livereload & add some build scripts

---
+++ reload.py
@@ -0,0 +1,12 @@
+import application
+
+from livereload import Server
+
+app = application.app
+app.debug = True
+server = Server(app.wsgi_app)
+
+server.watch('application.py')
+server.watch('static/dist')
+server.watch('templates')
+server.serve()
