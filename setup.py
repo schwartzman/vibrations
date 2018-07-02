849557aefb7f535ecd20d164d744d854764b4e49
Anthony Schwartzman <anthony@anthonyschwartzman.com>
2018-07-02 10:27:44 +0700

    Add some package meta stuff

---
+++ setup.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+from setuptools import find_packages
+from setuptools import setup
+
+readme = Path(__file__) / '..' / 'README.md'
+with readme.resolve().open() as f:
+    long_description = f.read()
+
+setup(
+    name='vibrations',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    url='https://github.com/schwartzman/vibrations',
+    author='Anthony Schwartzman',
+    author_email='anthony@anthonyschwartzman.com',
+    description='A minimalistic soundmap',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(exclude=[
+        'conf',
+        'lib',
+        'node_modules',
+        'static',
+        'templates'
+    ]),
+    install_requires=[
+        'boto3',
+        'flask',
+        'importlib_resources',
+        'pyyaml',
+        'requests'
+    ],
+    extras_require={
+        'dev': ['livereload']
+    }
+)
